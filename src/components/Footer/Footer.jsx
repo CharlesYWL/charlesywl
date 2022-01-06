038016cafe34280c813fafdc34c6ae05cc483e9c
charles-ar <weili.yin@authright.com>
2022-01-05 20:40:41 -0500

    Update Works

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,18 +1,18 @@
-import React, { Component } from "react";
-import ExpandLessIcon from "@material-ui/icons/ExpandLess";
-import "./index.sass";
-import { Avatar, IconButton } from "@material-ui/core";
+import React, { Component } from 'react';
+import ExpandLessIcon from '@material-ui/icons/ExpandLess';
+import './index.sass';
+import { Avatar, IconButton } from '@material-ui/core';
 
 class Footer extends Component {
   render() {
     return (
-      <div className="footer">
-        <div className="footer-backtotop">
+      <div className='footer'>
+        <div className='footer-backtotop'>
           <IconButton
             onClick={() => {
               document
-                .getElementById("home")
-                .scrollIntoView({ behavior: "smooth" });
+                .getElementById('home')
+                .scrollIntoView({ behavior: 'smooth' });
             }}
           >
             <Avatar style={{ height: 50, width: 50 }}>
@@ -20,7 +20,7 @@ class Footer extends Component {
             </Avatar>
           </IconButton>
         </div>
-        <div className="footer-version">Version 0.1.1</div>
+        <div className='footer-version'>Version 0.1.2</div>
       </div>
     );
   }
--- src/resumeData.js
+++ src/resumeData.js
@@ -1,46 +1,64 @@
-import ASLogo from "./components/img/AccessSecureLogo.png";
-import HMLogo from "./components/img/HalfMileLogo.png";
+import ASLogo from './components/img/AccessSecureLogo.png';
+import HMLogo from './components/img/HalfMileLogo.png';
 let resumeData = {
   home: {
-    bigTitle: "I am Weili Yin",
+    bigTitle: 'I am Weili Yin',
     discribtion:
-      "I am a Software Developer. I like dabbling in various parts of software development and like to learn edge technology, do some interesting projects or simply play games in my free time.",
-    linkedinLink: "https://www.linkedin.com/in/charlesywl/",
-    githubLink: "https://github.com/CharlesYWL",
+      'I am a Software Developer. I like dabbling in various parts of software development and like to learn edge technology, do some interesting projects or simply play games in my free time.',
+    linkedinLink: 'https://www.linkedin.com/in/charlesywl/',
+    githubLink: 'https://github.com/CharlesYWL',
   },
   about: {
     aboutMe:
-      "Now I'm seeking a front-end developer or DevOps position in California, but also willing to relocate elsewhere in the US, where exceptional knowledge and skills will be effectively put in agile software development process.",
-    imgUrl: "https://charlesywl.github.io/VegiExchange/charlesywl/weili.png",
+      "I am a full-stack developer with two years industrial experiences. Now I'm seeking a software developer position across US where exceptional knowledge and skills will be effectively put in agile software development process.",
+    imgUrl: 'https://charlesywl.github.io/VegiExchange/charlesywl/weili.png',
     contactDetails: {
-      email: "xlyin@ucdavis.edu",
-      phone: "530-304-9960",
+      email: 'xlyin@ucdavis.edu',
+      phone: '530-304-9960',
     },
   },
   works: [
     {
-      name: "Log Secure",
-      imgid: "../img/AccessSecureLogo.png",
-      link: "https://logsecure.io",
-      info:
-        "Middleware and service for access control, logging, anti-spamming, black/white list, ...",
-      tech: "React JS CSS",
+      name: 'Log Secure(Personal Project)',
+      imgid: '../img/AccessSecureLogo.png',
+      link: 'https://logsecure.io',
+      info: 'Middleware and service for access control, logging, anti-spamming, black/white list, ...',
+      tech: 'React JS CSS',
     },
     {
-      name: "Half Mile(Demo)",
-      imgid: "../img/HalfMileLogo.png",
-      link: "https://halfmile.io",
-      info:
-        "An All-In-One travel planner including route planning, hotel/flight reservation and auto-generated checklist",
+      name: 'Half Mile(Demo)',
+      imgid: '../img/HalfMileLogo.png',
+      link: 'https://halfmile.io',
+      info: 'An All-In-One travel planner including route planning, hotel/flight reservation and auto-generated checklist',
       tech: "React REST'API Firebase",
     },
     {
-      name: "Insta Like App",
-      imgid: "../img/WyInsta.png",
-      link: "http://www.wyinsta.com",
-      info:
-        "An Insta-like web applicaion that is equipped with majority funciton of Instagram",
-      tech: "Django AWS LightSail",
+      name: 'Insta Like App(Personal Project)',
+      imgid: '../img/WyInsta.png',
+      link: 'http://www.wyinsta.com',
+      info: 'An Insta-like web applicaion that is equipped with majority funciton of Instagram',
+      tech: 'Django AWS LightSail',
+    },
+    {
+      name: 'OnStar Insurance',
+      imgid: '../img/osi.jpg',
+      link: 'https://mp.midvaleinsurance.com/7146/quote/landing',
+      info: 'OnStar Insurance provides custom car insurance coverage plans that are intelligent, easy, and personal',
+      tech: 'TypeScript React/Redux',
+    },
+    {
+      name: 'Cmind-AI',
+      imgid: '../img/cmind.png',
+      link: 'https://www.cmind-ai.com/',
+      info: 'Cmind AI is a FinTech startup, focuses on developing an AI-powered platform for analyzing and predicting corporate financials using big data and machine learning technologies.',
+      tech: 'NodeJS React ElasticSearch',
+    },
+    {
+      name: 'CueThink',
+      imgid: '../img/cuethink.jpg',
+      link: 'https://www.cuethink.com/',
+      info: 'CueThink is an innovative application and a pedagogy focused on improving critical thinking skills and math',
+      tech: 'Django AWS LightSail',
     },
   ],
 };
